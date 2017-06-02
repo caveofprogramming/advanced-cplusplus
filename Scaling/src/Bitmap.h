9c814bddb215b9bef1a3f8f31c17cc32edae77bf
Paul T <developer.paul.123@gmail.com>
2017-06-01 22:11:37 -0400

    Added necessary string header
    Added the <string> header for some files that needed it (at least for Visual Studio).

--- Scaling/src/Bitmap.h
+++ Scaling/src/Bitmap.h
@@ -10,6 +10,8 @@
 
 #include <string>
 #include <cstdint>
+#include <memory>
+
 using namespace std;
 
 namespace caveofprogramming {
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
 #include "ring.h"
 using namespace std;
 
