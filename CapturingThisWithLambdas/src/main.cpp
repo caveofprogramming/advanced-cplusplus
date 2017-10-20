c960cb8e831563bac982dffcc0be3aaa560af1e3
John Purcell <john@caveofprogramming.com>
2017-10-20 21:36:57 +0100

    Added missing project code

--- CapturingThisWithLambdas/src/main.cpp
+++ CapturingThisWithLambdas/src/main.cpp
@@ -1,17 +1,32 @@
-//============================================================================
-// Name        :
-// Author      : John Purcell
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 
 using namespace std;
 
+class Test {
+private:
+	int one { 1 };
+	int two { 2 };
+
+public:
+	void run() {
+		int three { 3 };
+		int four { 4 };
+
+		auto pLambda = [&, this]() {
+			one = 111;
+			cout << one << endl;
+			cout << two << endl;
+			cout << three << endl;
+			cout << four << endl;
+		};
+		pLambda();
+	}
+
+};
+
 int main() {
-	cout << "Hello World" << endl;
+	Test test;
+	test.run();
 
 	return 0;
 }
--- TheFractalAlgorithm/src/main.cpp
+++ TheFractalAlgorithm/src/main.cpp
@@ -1,17 +1,48 @@
 //============================================================================
-// Name        :
-// Author      : John Purcell
+// Name        : Fractal.cpp
+// Author      : 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
+#include <cstdint>
+#include "Mandelbrot.h"
+#include "Bitmap.h"
 
 using namespace std;
+using namespace caveofprogramming;
 
 int main() {
-	cout << "Hello World" << endl;
 
+	int const WIDTH = 800;
+	int const HEIGHT = 600;
+
+	Bitmap bitmap(WIDTH, HEIGHT);
+
+	double min = 999999;
+	double max = -999999;
+
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			double xFractal = (x - WIDTH/2) * 2.0/WIDTH;
+			double yFractal = (y - HEIGHT/2) * 2.0/HEIGHT;
+
+			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
+
+			uint8_t red = (uint8_t)(256 * (double)iterations/Mandelbrot::MAX_ITERATIONS);
+
+			bitmap.setPixel(x, y, red, red, red);
+			if(red < min) min = red;
+			if(red > max) max = red;
+		}
+	}
+
+	cout << min << ", " << max << endl;
+
+	bitmap.write("test.bmp");
+
+	cout << "Finished." << endl;
 	return 0;
 }
